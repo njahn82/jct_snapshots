9df042767ce96ac81d24c8388ac09d6bbea284e7
najahn <najko.jahn@gmail.com>
2024-03-27 13:52:25 +0100

    Initial fetch

---
+++ R/jct_fetch.R
@@ -0,0 +1,54 @@
+#' Helper function to retrieve and parse Transformative Agreement Data
+#'
+#' @param esac_id Transformative Agreement ID issued by ESAC
+#' @param data_url URL to Google Spreadsheet
+#' 
+#' @importFrom dplyr select mutate filter
+#' @export
+jct_fetch <- function(esac_id = NULL, data_url = NULL) {
+  # Progress
+  message(paste("Fetching:", esac_id, "from", data_url))
+  # Download spreadsheet
+  req <- readr::read_csv(
+    data_url,
+    col_types = "cccccccc",
+    show_col_types = FALSE,
+    progress = FALSE
+  )
+  
+  # Journal-level data
+  jn_df <- req |>
+    dplyr::select(
+      journal_name = 1,
+      issn_print = 2,
+      issn_online = 3,
+      first_seen = 4,
+      last_seen = 5
+    ) |>
+    dplyr::filter(!is.na(journal_name)) |>
+    dplyr::mutate(esac_id = esac_id)
+  
+  # Institutional-level data
+  inst_df <- req |>
+    dplyr::select(
+      inst_name = 6,
+      ror_id = 7,
+      inst_first_seen = 8,
+      inst_last_seen = 9
+    ) |>
+    dplyr::filter(!is.na(inst_name)) |>
+    dplyr::mutate(esac_id = esac_id)
+  
+  # Return
+  list(jn_df = jn_df, inst_df = inst_df)
+}
+
+jct_overview <- function(data_url = jct_url()) {
+  readr::read_csv(data_url) |>
+    janitor::clean_names()
+}
+
+
+jct_url <-
+  function()
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vStezELi7qnKcyE8OiO2OYx2kqQDOnNsDX1JfAsK487n2uB_Dve5iDTwhUFfJ7eFPDhEjkfhXhqVTGw/pub?gid=1130349201&single=true&output=csv"
---
+++ data-raw/get_jct_data.R
@@ -0,0 +1,21 @@
+# Gather Transformative Agreement Public Data 
+pkgload::load_all()
+# Overview
+jct_overview <- jct_overview()
+# Get data per agreement
+jct_req <- 
+  purrr::map2(jct_overview$esac_id[1:10], 
+              jct_overview$data_url[1:10],
+              purrr::safely(jct_fetch)
+              )
+# Save
+path <- paste0(here::here("data-raw"), "/", Sys.Date())
+fs::dir_create(path)
+# Save journal data
+purrr::map(jct_req, "result") |>
+  purrr::map_df("jn_df") |>
+  readr::write_csv(paste0(path, "/jct_journals.csv"))
+# Save inst data
+inst_df <- purrr::map(jct_journal_out, "result") |>
+  purrr::map_df("inst_df") |>
+  readr::write_csv("data/jct_institutions.csv")
